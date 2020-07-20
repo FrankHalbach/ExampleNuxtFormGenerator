06ca1adf1d1ca2677faf7c2ed6bf7366ee736c71
Frank Halbach <frank.halbach@visteon.com>
2020-07-20 12:25:12 +0200

    adding index page

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card>
+    <v-card-title primary-title>
+      <div>
+        <div class="headline">Admin</div>
+      </div>
+    </v-card-title>
+    <v-card-actions>
+      <v-btn to="/customers" text>Customers</v-btn>
+      <v-btn to="/plants" text>Plants</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
